3ed67db6ecd063c0652c1f48f421c4a5e2c340de
wendellcs <wendellcs131@gmail.com>
2026-07-30 17:30:19 -0300

    feat: create /classes/find route for getting student's class_id

--- routers/classes.py
+++ routers/classes.py
@@ -1,9 +1,25 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from schemas.classesSchema import ClassesSchema
 import psycopg2
+from datetime import time
 from database import get_connection
 
 router = APIRouter(prefix='/classes')
+
+@router.get('/find', status_code=status.HTTP_200_OK)
+def find_class(module:str, day_of_week:str, class_time:time , db = Depends(get_connection)):
+    _, cursor = db
+
+    cursor.execute('SELECT id FROM classes WHERE module=%s AND day_of_week=%s AND class_time=%s',
+        (module, day_of_week, class_time))
+    
+    class_id = cursor.fetchone()
+    
+    if class_id:
+        return class_id[0]
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Class not found.')
+   
 
 @router.post('/add', status_code = status.HTTP_201_CREATED)
 def create_class(class_data: ClassesSchema, db = Depends(get_connection)):
